ff37bc9ef8f2ae2a5d460e23def186413bc9bbe3
oasg <gouruibo98765@outlook.com>
2024-06-17 20:55:09 +0900

    cli program & and gcc o3 native flags

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,4 +1,15 @@
-
+#include "Simulator.h"
+#include "model/HairMultilayerModel.h"
 int main(){
-
+    auto builder = std::make_shared<ModelBuilder>("/home/oasis/Documents/code/NS-FTDT/scene/model/hairmodel.json");
+    builder->buildModel();
+    auto sim = std::make_shared<Simulator>(TYPE::sim_type::NsFTDT_TE,builder);
+    while (true)
+    {
+        
+        int res = sim->calc();
+        if(res==0){
+            break;
+        }
+    }
 }
