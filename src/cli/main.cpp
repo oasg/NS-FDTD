ab6b9895c9e87766dd4219be3dcee838566ea0ee
oasg <gouruibo98765@outlook.com>
2025-02-01 20:53:37 +0900

    add cmdline to cli program

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,15 +1,27 @@
 #include "Simulator.h"
 #include "model/HairMultilayerModel.h"
-int main(){
-    auto builder = std::make_shared<ModelBuilder>("../scene/model/hairmodelRandom.json");
-    builder->buildModel();
-    auto sim = std::make_shared<Simulator>(TYPE::sim_type::NsFTDT_TM,builder);
-    while (true)
-    {
-        
-        int res = sim->calc();
-        if(res==0){
-            break;
+#include "cmdline.h"
+#include <iostream>
+
+
+int main(int argc, char *argv[]){
+    cmdline::parser parser;
+    parser.add<std::string>("model", 'm', "model file path", true, "");
+    parser.parse_check(argc, argv);
+    std::cout << parser.get<std::string>("model") << std::endl;
+    if(parser.exist("model")){
+        auto builder = std::make_shared<ModelBuilder>(parser.get<std::string>("model"));
+        builder->buildModel();
+        auto sim = std::make_shared<Simulator>(TYPE::sim_type::NsFTDT_TM,builder);
+        while (true)
+        {
+            int res = sim->calc();
+            if(res==0){
+                break;
+            }
         }
+    }
+    else{
+        std::cout << "Please specify the model file path" << std::endl;
     }
 }
